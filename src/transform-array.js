95fb3983651b6aaa5a03daf4037a68a422323399
Valeriia Galakhova <galakhova.valeriya@gmail.com>
2023-04-14 23:51:32 +0400

    feat: add transform array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,32 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("\'arr\' parameter must be an instance of the Array!");
+  }
+  let res = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "--discard-next") {
+      i += 2;
+    } else if (arr[i] === "--discard-prev") {
+      res.pop();
+    } else if (arr[i] === "--double-next") {
+      if(arr[i+1] !== undefined){
+      res.push(arr[i + 1])} else {i}
+    } else if (arr[i] === "--double-prev") {
+      if (arr[i - 1] !== undefined) {
+        res.push(arr[i - 1]);
+      } else {
+        i;
+      }
+    } else {
+      res.push(arr[i]);
+    }
+  }
+
+  return res;
 }
 
 module.exports = {
