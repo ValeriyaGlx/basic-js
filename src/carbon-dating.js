49746a44831bcad80766cc5bdaaa33ad5a27ca2a
Valeriia Galakhova <galakhova.valeriya@gmail.com>
2023-04-14 18:00:06 +0400

    fix: solve carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,9 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(string) {
+  if (typeof string !== 'string' || Number.isNaN(string*1) || string*1 > 15 || string*1<=0) {return false}
+  return Math.ceil(HALF_LIFE_PERIOD/Math.LN2 * Math.log(MODERN_ACTIVITY/string*1));
 }
 
 module.exports = {
