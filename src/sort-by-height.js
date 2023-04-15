3427b59af57a4321485b4ef85033150ab0013f61
Valeriia Galakhova <galakhova.valeriya@gmail.com>
2023-04-16 01:42:23 +0400

    feat solve

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  const unit = arr.filter((el) => el !== -1).sort((a, b) => a - b);
+  const res = [];
+  console.log(unit);
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === -1) {
+      res[i] = -1;
+    } else {
+      res[i] = unit.shift();
+    }
+  }
+  return res;
 }
 
 module.exports = {
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,9 +12,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+  let arr = (n + "").split("");
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i] * 1;
+  }
+  arr = (n + "").split("");
+  if (arr.length !== 1) {
+    return getSumOfDigits(sum);
+  }
+
+  return sum;
 }
 
 module.exports = {
